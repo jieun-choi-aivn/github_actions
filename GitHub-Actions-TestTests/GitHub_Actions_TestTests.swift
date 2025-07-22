5bfcccdb2496d7752cbef1c160c7d64cf56cc207
jieun-choi-aivn <jieun.choi@aivenautics.com>
2025-07-22 10:05:22 +0900

    first commit

---
+++ GitHub-Actions-TestTests/GitHub_Actions_TestTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitHub_Actions_TestTests.swift
+//  GitHub-Actions-TestTests
+//
+//  Created by jechoi on 7/22/25.
+//
+
+import XCTest
+@testable import GitHub_Actions_Test
+
+final class GitHub_Actions_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
